533f476d955eac2733589eb32d0104f9d7ec3bea
cmq2080 <1421224516@qq.com>
2025-11-09 17:20:23 +0800

    feat: 项目结构调整 (refs: #251109-1720)

--- src/facade/Response.php
+++ src/facade/Response.php
@@ -13,6 +13,7 @@ use liansu\Facade;
  * @method string json($stat, $msg = '', $data = [])
  * @method string success($data = [], $msg = 'OK')
  * @method string error($msg = 'ERR', $stat = 1, $data = [])
+ * @method string isInFormat($content)
  */
 class Response extends Facade
 {
